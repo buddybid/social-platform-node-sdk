eda24d73923eff6ffcbb0cbfaa47a8762c77c271
Jake <jake@buddybid.com>
2016-11-03 14:22:09 +1300

    bug fix

--- tests/buddybid.test.js
+++ tests/buddybid.test.js
@@ -6,29 +6,21 @@ var BB = null;
 describe('BuddyBid Social Platform API', function() {
 
     beforeEach(function(done) {
-
-        BB = new BuddyBid({ baseEndpoint:'dev-tim-backend.internal.buddybid.com:3000',
-                            ownerId: '56a687ce13e2df0875b457e1',
-                            apiKey:'98a80d3638be5623be4a559b8f96a398'});
-
+        BB = new BuddyBid({ baseEndpoint:'dev-jake.internal.buddybid.com:3000',
+                            ownerId: '5750ff93a235aeb11ac21e27',
+                            apiKey:'a9895957bb5e2749ad1401745c557ff3'});
         done();
     });
 
-
-
     describe('BB instance', function () {
         it('bb should be function', function (done) {
-
             BuddyBid.should.be.type('function');
             done();
-
         });
 
         it('should init buddybid api lib', function (done) {
-
             BB.should.be.type('object');
             done();
-
         });
 
         it('request is a function.', function(done) {
@@ -46,7 +38,7 @@ describe('BuddyBid Social Platform API', function() {
             done();
         });
 
-        it('patch is a function.', function(done) {
+        it('put is a function.', function(done) {
             BB.delete.should.be.type('function');
             done();
         });
@@ -55,20 +47,25 @@ describe('BuddyBid Social Platform API', function() {
             BB.post.should.be.type('function');
             done();
         });
-
-        /*it('get request', function(done) {
-            BB.get('/owner/56a687ce13e2df0875b457e1', function(err, body) {
-                done();
-            });
-
-        });*/
-        
-        
     });
 
+    describe('Request Methods', function () {
+        it('GET request', function(done) {
+            BB.get('/owner/5750ff93a235aeb11ac21e27', function(err, body) {
+                should.not.exists(err);
+                done();
+            });
+        });
+
+        it('DELETE request', function(done) {
+            BB.delete('/users/57510d0dc4197d101b754da8/watchlist/581a8e1168e897850de8ea6d', function(err, body) {
+                should.not.exists(err);
+                done();
+            });
+        });
+    });
 
     afterEach(function(done) {
-
         done();
     });
 });
